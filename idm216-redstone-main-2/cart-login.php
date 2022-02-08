12d0e7ad92416b58c6469dab56cafa2ba1d1cdc8
Joey-949 <71980853+Joey-949@users.noreply.github.com>
2022-02-08 17:50:39 -0500

    Add files via upload

---
+++ idm216-redstone-main-2/cart-login.php
@@ -0,0 +1,26 @@
+<?php
+include_once __DIR__ . '/header.php';
+?>
+
+<div>
+    <h3 class="align-center-text">Pick-up at: Drexel University</h3>
+</div>
+
+<?php
+include_once __DIR__ . '/cart.php';
+?>
+
+<div class="pop-up white-background">
+    <a href="cart.php" class="close"><img src="https://via.placeholder.com/32"></a>
+    <h2>Login to Account</h2>
+    <input class="login-text" type="text" placeholder="Username">
+    <input class="login-text" type="text" placeholder="Password">
+    <a class="google-login" href="google.com">Login with Google</a>
+    <a href="payment.php" id="confirm-pick-up-time">Confirm</a>
+    <div>
+    </div>
+
+
+    <?php
+    include_once __DIR__ . '/footer.php';
+    ?>
